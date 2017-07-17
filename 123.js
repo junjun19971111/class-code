c240c59f5315859c5a6c780423d9210a3985de42
junjun <1121968314@qq.com>
2017-07-17 00:02:16 -0400

    5

---
+++ 123.js
@@ -0,0 +1,29 @@
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    introduce(){
+        return 'My name is '+this.name+'.I am '+this.age+' years old';
+    }
+}
+class Student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    introduce(){
+        return super.introduce()+`. I am at class ${this.klass}.`;
+    }
+}
+class Worker extends Person{
+    introduce(){
+        return  super.introduce()+`I have a job.`;
+    }
+}
+let person=new Person('Tom',21);
+let student=new Student('Tom',21,2);
+let worker=new Worker('Tom',21);
+console.log(person.introduce());
+console.log(student.introduce());
+console.log(worker.introduce());
