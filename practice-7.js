a43712defe995d83fa32eb9760ce8aebe01c67ab
junjun <1121968314@qq.com>
2017-07-17 01:55:24 -0400

    7

---
+++ practice-7.js
@@ -0,0 +1,38 @@
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    introduce(){
+        return 'My name is '+this.name+'.I am '+this.age+' years old';
+    }
+}
+class Student extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    introduce(){
+        return super.introduce()+`. I am at class ${this.klass}.`;
+    }
+}
+class Teacher extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    introduce(){
+        if(this.klass){
+            return super.introduce() + `I teach at class ${this.klass}`;
+        }
+        else {
+            return super.introduce() + `.I teach at no class.`;
+        }
+    }
+}
+let person=new Person('Tom',21);
+let student=new Student('Tom',21,2);
+let teacher=new Teacher('Tom',21);
+console.log(person.introduce());
+console.log(student.introduce());
+console.log(teacher.introduce());
